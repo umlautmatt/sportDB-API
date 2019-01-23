0bbdd0d7fe4852c9aded45aca5c6aba3880ccd80
Matt Hawley <matthawley@Matts-MacBook-Pro.local>
2019-01-23 09:29:37 -0500

    updating API project

--- sport.js
+++ sport.js
@@ -3,61 +3,65 @@ const key = '1';
 let url;
 
 const searchCity= document.querySelector('.city');
-const searchName =document.querySelector(".name");
+const searchName =document.querySelector('.name');
 const searchForm = document.querySelector('form');
 const submitBtn = document.querySelector('.submit');
 
-const teamList = document.querySelector('ul');
+const teamList = document.querySelector('section');
 
 searchForm.addEventListener('submit', fetchResults);
 
 function fetchResults(e) { //function is ran when searchForm from previous section is "submit" 
     e.preventDefault();
-    url = baseurl + 'searchteams.php?t=' + searchCity.value + searchName.value;
+    url = baseurl + 'searchteams.php?t=' + searchCity.value 
+    // + searchName.value;
     console.log(url)
     
     fetch(url)
-      .then(function(response) {
-          console.log(response);
-        // return response.json();
+      .then(function(result) {
+          console.log(result);
+        return result.json();
         })
         .then(function(json) {
             console.log(json);
         displayResults(json); 
         });
-      }
-      function displayResults(json) {
-        let teamResults = json.teams[0].strTeam;
-        console.log(teamResults);
+}
+function displayResults(json) {
+  while (teamList.firstChild) {
+    teamList.removeChild(teamList.firstChild);//
+}
+    // let teamResults = json.teams[0].strTeam;
+    let teamResults = json.teams;
+    console.log(teamResults);
+    
+    
+    for(let i = 0; i < teamResults.length; i++) {
+      let article = document.createElement('article');
+      let heading = document.createElement('h2');
+      let manager = document.createElement('h1');
+      let stadium = document.createElement('h2');
+      let para = document.createElement('p');
+      let clearfix = document.createElement('div');
+
+      let current = teamResults[i];
+      console.log("Current:", current);
+      
+      heading.textContent = current.strTeam;
+      manager.textContent = 'Head Coach: ' + current.strManager;
+      stadium.textContent = 'Stadium: ' + current.strStadium;
+      para.textContent = current.strDescriptionEN;
+      // console.log()
+      
+      article.appendChild(heading);
+      article.appendChild(stadium); 
+      article.appendChild(manager);
+      article.appendChild(para);
+      article.appendChild(clearfix);
+      teamList.appendChild(article);
+    }
+    
   };
-  
-  
-  // for(let i = 0; i < teamResults.length; i++) {
-  //   let article = document.createElement('article');
-  //   let heading = document.createElement('h2');
-  //   let link = document.createElement('a');
-  //   let img = document.createElement('img');
-  //   let para = document.createElement('p');
-  //   let clearfix = document.createElement('div');
-  //   let span = document.createElement('span');
-    
-  //   let current = teamResults[i];
-    
-  //   span.textContent += current.teamResults[i].value + '';
-  //   para.appendChild(span);
-  //   console.log(current);
-
-
-
-  //   article.appendChild(heading);
-  //   heading.appendChild(link);
-  //   article.appendChild(img); 
-  //   article.appendChild(para);
-  //   article.appendChild(clearfix);
-  //   section.appendChild(article);
-  // }
-// };
-
 
 
 
